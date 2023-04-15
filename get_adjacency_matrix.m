31db8fc8d59b1033813cb5c4a030cdd02263c496
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-04-15 19:01:47 +0300

    am adaugat win/loose

--- get_adjacency_matrix.m
+++ get_adjacency_matrix.m
@@ -5,8 +5,14 @@ function [Adj] = get_adjacency_matrix(Labyrinth)
     % N este numarul de celule din matrice fara Win si Loose(nr de noduri)
     N = m * n;
 
-    % cream matricea rara de adiacenta Adj
-    Adj = sparse(N, N);
+    % cream matricea rara de adiacenta Adj (labirint + Win & Loose)
+    Adj = sparse(N + 2, N + 2);
+
+    % din Win in Win
+    Adj(N + 1, N + 1) = 1;
+
+    % din Loose in Loose
+    Adj(N + 2, N + 2) = 1;
 
     % parcurgem fiecare celula din labirint
     % marcam in matricea de adiacenta pozitiile in care poate sa ajunga robotul
@@ -22,27 +28,58 @@ function [Adj] = get_adjacency_matrix(Labyrinth)
 
             % vecinul de sus
             bit = bitand(current, bitshift(1,3));
+
+            % marcam pentru Win sus
+            if i == 1 && bit == 0
+              Adj(k, N + 1) = 1;
+            endif
+
+            % marcam pentru interior
             if in_matrix(m, n, i-1, j) ~= 0 && bit == 0
               k_top = (i-2)*n + j;
               Adj(k, k_top) = 1;
             endif
 
+
             % vecinul de jos
             bit = bitand(current, bitshift(1,2));
+
+            % marcam pentru Win jos
+            if i == m && bit == 0
+              Adj(k, N + 1) = 1;
+            endif
+
+            % marcam pentru interior
             if in_matrix(m, n, i+1, j) ~= 0 && bit == 0
               k_bottom = i*n + j;
               Adj(k, k_bottom) = 1;
             endif
 
+
             % vecinul din dreapta
             bit = bitand(current, bitshift(1,1));
+
+            % marcam pentru loose dreapta
+            if j == n && bit == 0
+              Adj(k, N + 2) = 1;
+            endif
+
+            % marcam pentru interior
             if in_matrix(m, n, i, j+1) ~= 0 && bit == 0
               k_right = (i-1)*n + j+1;
               Adj(k, k_right) = 1;
             endif
 
+
             % vecinul din stanga
             bit = bitand(current, bitshift(1,0));
+
+            % marcam pentru loose stanga
+            if j == 1 && bit == 0
+              Adj(k, N + 2) = 1;
+            endif
+
+            % marcam pentru interior
             if in_matrix(m, n, i, j-1) ~= 0 && bit == 0
               k_left = (i-1)*n + j-1;
               Adj(k, k_left) = 1;
